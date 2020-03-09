47f5ffbf0e68283d071fc74531b85689488c49c8
Oby Manyando <33107568+obymanyando@users.noreply.github.com>
2020-03-09 14:08:57 +0200

    new branch - addpst

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import AddPost from './components/AddPost';
 
 function App() {
   return (
     <div className="App">
       App
+      <AddPost />
     </div>
   );
 }
---
+++ src/components/AddPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AddPost extends Component {
+    render() {
+        return (
+            <div>
+                <h1>New Post</h1>
+            </div>
+        )
+    }
+}
+
+export default AddPost
