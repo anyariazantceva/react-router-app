48ea7e5d7c2d5a9d5ac501b026d95d02b37cd52d
chaeahpark <parkchaeah331@gmail.com>
2020-03-10 14:02:06 +0200

    test to add the edit and delete btns

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,29 +1,44 @@
 import React from 'react';
-import PostItem from "./PostItem";
+import PostItem from './PostItem';
+import PostButtons from './PostButtons';
 import './main-page.css';
 
+const MainPage = ({ posts, userId }) => {
+  const postsList = posts.map(post => {
+    if (userId === post.userId) {
+      return (
+        <div>
+          <PostItem key={post.id} title={post.title} category={post.category} />
+          <div>button</div>
+        </div>
+      );
+    }
+    return (
+      <PostItem key={post.id} title={post.title} category={post.category} />
+    );
+  });
+  //   console.log(posts);
+  //   const  renderPosts = () => {
+  //     const filteredPosts = posts.filter(post => userId === post.userId);
+  //     console.log(filteredPosts);
+  //     filteredPosts.map(post => {
+  //       console.log(post);
+  //       return (
+  //         <div>
+  //           <PostItem key={post.id} title={post.title} category={post.category} />
+  //           <PostButtons />
+  //         </div>
+  //       );
+  //     });
+  //   };
 
-const MainPage = ({posts, userId}) => {
-        // const postsList = posts.map((post) => {
-        //     return <PostItem key={post.id} title={post.title} category={post.category}/>
-        // });
-        const renderPosts = () => {
-            posts.map((post) => {
-                if(userId === post.userId) {
-                    return <PostItem key={post.id} title={post.title} category={post.category}/>
-                }
-            })
-        }
-        return (
-            <div className='container'>
-                <div className="main__title">Posts List</div>
-                <button className="btn">Add Post</button>
-                <div className="posts__list">
-                    {renderPosts()}
-                </div>
-            </div>
-        )
-
+  return (
+    <div className="container">
+      <div className="main__title">Posts List</div>
+      <button className="btn">Add Post</button>
+      <div className="posts__list">{postsList}</div>
+    </div>
+  );
 };
 
-export default MainPage
+export default MainPage;
