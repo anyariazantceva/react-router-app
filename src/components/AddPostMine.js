e59cb54c6517d4f3b72a881189f393028b4cee77
chaeahpark <parkchaeah331@gmail.com>
2020-03-10 15:51:15 +0200

    create the function to add a post

--- src/components/AddPostMine.js
+++ src/components/AddPostMine.js
@@ -1,92 +1,89 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class AddPostMine extends Component {
-	constructor(props) {
-		super(props)
-	
-		this.state = {
-			title: '',
-			category: '',
-			content: '',
-			key: '',
-		}
-	}
+  constructor(props) {
+    super(props);
 
-	handleChange = e => {
-		const { name, value } = e.target
+    this.state = {
+      title: '',
+      category: '',
+      content: '',
+      key: ''
+    };
+  }
 
-		this.setState({ [name]: value })
-		console.log(e.target.value)
-		// const value = e.target.value
-		// const name = e.target.name
-		// this.setState({title : e.target.value})
-	}
-	
-	handleSubmit = e => {
-		e.preventDefault()
-		const newPost = {
-			title: this.state.title,
-			category: this.state.category,
-			content: this.state.content,
-			key: Date.now(),
-		}
-		let newPosts = []
-		
-		newPosts = this.props.posts.push(newPost)
-		this.props.addPost(newPosts)
-		console.log(newPosts)
+  handleChange = e => {
+    const { name, value } = e.target;
 
-	}
+    this.setState({ [name]: value });
+    console.log(e.target.value);
+    // const value = e.target.value
+    // const name = e.target.name
+    // this.setState({title : e.target.value})
+  };
 
-	render() {
-		console.log(this.props.posts)
+  handleSubmit = e => {
+    e.preventDefault();
 
-		return (
-			<div className='form__container'>
-				<h2>{this.state.title}</h2>
-				<h2>{this.state.category}</h2>
-				<h2>{this.state.content}</h2>
-				<form onSubmit={this.handleSubmit}>
-					<div className='title'>
-						<label htmlFor='title'>Title:</label>
-						<input
-							id='title'
-							name='title'
-							value={this.state.title}
-							onChange={this.handleChange}
-							placeholder='Title'
-							type='text'
-						/>
-					</div>
-					<div className='category'>
-						<label htmlFor='Category'>Category:</label>
-						<input
-							id='category'
-							name='category'
-							value={this.state.category}
-							onChange={this.handleChange}
-							placeholder='Category'
-							type='text'
-						/>
-					</div>
-					<div className='writeNewField'>
-						<label htmlFor='writeNewField'>write New Post:</label>
-						<textarea
-							name='content'
-							id='textfield'
-							value={this.state.content}
-							onChange={this.handleChange}
-							cols='30'
-							rows='10'></textarea>
-					</div>
-					<button className='btn--save'>
-						Save
-					</button>
-					<button>Cancel</button>
-				</form>
-			</div>
-		)
-	}
+    const newPost = {
+      title: this.state.title,
+      category: this.state.category,
+      content: this.state.content,
+      key: Date.now()
+    };
+
+    this.props.addPost(newPost);
+    // console.log(newPosts)
+  };
+
+  render() {
+    console.log(this.props.posts);
+
+    return (
+      <div className="form__container">
+        <h2>{this.state.title}</h2>
+        <h2>{this.state.category}</h2>
+        <h2>{this.state.content}</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div className="title">
+            <label htmlFor="title">Title:</label>
+            <input
+              id="title"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+              placeholder="Title"
+              type="text"
+            />
+          </div>
+          <div className="category">
+            <label htmlFor="Category">Category:</label>
+            <input
+              id="category"
+              name="category"
+              value={this.state.category}
+              onChange={this.handleChange}
+              placeholder="Category"
+              type="text"
+            />
+          </div>
+          <div className="writeNewField">
+            <label htmlFor="writeNewField">write New Post:</label>
+            <textarea
+              name="content"
+              id="textfield"
+              value={this.state.content}
+              onChange={this.handleChange}
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+          <button className="btn--save">Save</button>
+          <button>Cancel</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default AddPostMine
+export default AddPostMine;
--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,54 @@
-import React, { Component } from 'react'
-import './App.css'
-import AddPostMine from './AddPostMine'
-import Header from './Header'
-import MainPage from './MainPage'
-import posts from './posts'
+import React, { Component } from 'react';
+import './App.css';
+import AddPostMine from './AddPostMine';
+import Header from './Header';
+import MainPage from './MainPage';
+import posts from './posts';
 
 class App extends Component {
-	// The states are related with
-	// the GoogleAuth Component
-	state = {
-		isSignedIn: null,
-		userId: null,
-		posts: posts,
-		title: '',
-		category: '',
-		content: '',
-	}
+  // The states are related with
+  // the GoogleAuth Component
+  state = {
+    isSignedIn: null,
+    userId: null,
+    posts: posts,
+    title: '',
+    category: '',
+    content: ''
+  };
 
-	// This method is passed as props
-	// down to the GoogleAuth Component
-	updateSignInStatus = isSignedIn => {
-		this.setState({ isSignedIn: isSignedIn })
-	}
+  // This method is passed as props
+  // down to the GoogleAuth Component
+  updateSignInStatus = isSignedIn => {
+    this.setState({ isSignedIn: isSignedIn });
+  };
 
-	// This method is passed as props
-	// down to the GoogleAuth Component
-	updateUserId = id => {
-		this.setState({ userId: id })
-	}
+  // This method is passed as props
+  // down to the GoogleAuth Component
+  updateUserId = id => {
+    this.setState({ userId: id });
+  };
 
-	addPost = newPosts => {
-		this.setState({ posts: newPosts })
-	}
+  addPost = newPost => {
+    let newPostList = [...this.state.posts, newPost];
+    this.setState({ posts: newPostList });
+  };
 
-	render() {
-		return (
-			<div className='ui container'>
-				<div>
-					<Header
-						isSignedIn={this.state.isSignedIn}
-						updateSignIn={this.updateSignInStatus}
-						updateUserId={this.updateUserId}
-					/>
-					<MainPage posts={this.state.posts} />
-					<AddPostMine posts={this.state.posts} addPost={this.addPost} />
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          <Header
+            isSignedIn={this.state.isSignedIn}
+            updateSignIn={this.updateSignInStatus}
+            updateUserId={this.updateUserId}
+          />
+          <MainPage posts={this.state.posts} />
+          <AddPostMine posts={this.state.posts} addPost={this.addPost} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
